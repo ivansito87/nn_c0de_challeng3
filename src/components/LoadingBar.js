7df2eb9a36b5710a253aa17f29752a636864195e
Ivan Rendon <ivansito87@gmail.com>
2020-07-27 16:50:58 -0400

    Update LoadingBar.js

--- src/components/LoadingBar.js
+++ src/components/LoadingBar.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 class LoadingBar extends React.Component {
-    render() {
-        return (
-            <div style={{ width: "100%" }}>
-                <p>Loading...</p>
-                <div className="progress">
-                    <span className="progress-bar" style={{ width: '100%' }}></span>
-                </div>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div style={{ width: "100%" }}>
+				<p>Loading...</p>
+				<div className="progress">
+					<span className="progress-bar" style={{ width: '100%' }}></span>
+				</div>
+			</div>
+		);
+	}
 }
 export default LoadingBar;
