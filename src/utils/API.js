3a9470b8a1d1f9ae20434dbbed8c4f616f94b20b
Ivan Rendon <ivansito87@gmail.com>
2020-07-27 16:50:39 -0400

    Update API.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -6,4 +6,14 @@ export default {
     getTweets: function () {
         return axios.get("/api/cnn-tweets");
     },
+
+    // Handles the post to search term
+    setSearchTerm: function (term) {
+        return axios.post('/setSearchTerm', { term });
+    },
+
+    pauseStream: function () {
+        return axios.post("/pause", {})
+    }
+
 }
