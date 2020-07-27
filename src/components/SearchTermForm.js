d03f16756b90b8cd06ce79a98a05321e21fbe215
Ivan Rendon <ivansito87@gmail.com>
2020-07-27 16:50:51 -0400

    Update SearchTermForm.js

--- src/components/SearchTermForm.js
+++ src/components/SearchTermForm.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Form, FormControl, Button } from 'react-bootstrap';
 class FormSearchController extends React.Component {
-    render() {
-        return (
-            <Form inline>
-                <FormControl type="text" placeholder="CNN" className="mr-sm-2" onChange={this.props.handleChange} />
-                <Button variant="outline-success" onClick={this.props.handleResume}>Search</Button>
-            </Form>
-        )
-    }
+	render() {
+		return (
+			<Form inline>
+				<FormControl type="text" placeholder="CNN" className="mr-sm-2" onChange={this.props.handleChange} />
+				<Button variant="outline-success" onClick={this.props.handleResume}>Search</Button>
+			</Form>
+		)
+	}
 }
 export default FormSearchController;
