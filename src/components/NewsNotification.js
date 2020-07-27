4f02b382440a770b597248c48e44a50473bf47e2
Ivan Rendon <ivansito87@gmail.com>
2020-07-27 16:50:54 -0400

    Update NewsNotification.js

--- src/components/NewsNotification.js
+++ src/components/NewsNotification.js
@@ -3,30 +3,28 @@ import { Alert, Button } from 'react-bootstrap';
 
 class NewsNotification extends React.Component {
 
-    state = {
-        show: false,
-    };
+	state = {
+		show: false,
+	};
 
-    render() {
-        let data = this.props.data;
-
-        return (
-            <React.Fragment>
-                <Alert show={!this.state.show} variant="danger" style={{ marginBottom: 0 }}>
-                    <Alert.Heading>BREAKING NEWS</Alert.Heading>
-                    <p>
-                        Millennials are fleeing cities and waging bidding
-                        wars for suburban and even rural homes. It's a
-                        big bet that the work-from-home lifestyle is here to stay
-                    </p>
-                    <div className="d-flex justify-content-end">
-                        <Button onClick={() => this.setState({ show: true })} variant="outline-danger">
-                            close
-                        </Button>
-                    </div>
-                </Alert>
-            </React.Fragment>
-        )
-    }
+	render() {
+		return (
+			<React.Fragment>
+				<Alert show={!this.state.show} variant="danger" style={{ marginBottom: 0 }}>
+					<Alert.Heading>BREAKING NEWS</Alert.Heading>
+					<p>
+						Millennials are fleeing cities and waging bidding
+						wars for suburban and even rural homes. It's a
+						big bet that the work-from-home lifestyle is here to stay
+          </p>
+					<div className="d-flex justify-content-end">
+						<Button onClick={() => this.setState({ show: true })} variant="outline-danger">
+							close
+            </Button>
+					</div>
+				</Alert>
+			</React.Fragment>
+		)
+	}
 }
 export default NewsNotification;
